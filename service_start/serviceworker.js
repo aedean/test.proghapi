168f6257632234c49e7f3ca477dae2f74c4916bd
DEAN Alice <d016761f@student.staffs.ac.uk>
2017-03-14 11:17:23 +0000

    Service worker changes js

--- service_start/serviceworker.js
+++ service_start/serviceworker.js
@@ -2,4 +2,11 @@ self.addEventListener('fetch', function(event) {
   console.log('Fetch request for:', event.request.url);
 });
 
-console.log("here");
+self.addEventListener('fetch', function(event) {
+  if (event.request.url.indexOf('material.teal-red.min.css') !== -1) {
+    event.respondWith(
+      new Response('body { background: green; }',
+      { headers: { 'Content-Type': 'text/css' } }
+    ));
+  }
+});
