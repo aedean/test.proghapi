d72fdcff383fdcadcfa9ecd8ba366fc4853e38b9
DEAN Alice <d016761f@student.staffs.ac.uk>
2017-03-21 10:07:52 +0000

    21/3 service worker green bar

--- service_start/serviceworker.js
+++ service_start/serviceworker.js
@@ -19,9 +19,8 @@ self.addEventListener('fetch', function(event) {
     );
   }
 })
-*/
 
-var CACHE_NAME = 'gih-cache';
+var CACHE_NAME = 'gih-cache-v2';
 var CACHED_URLS = [
   'offline.html',
   'mystyles.css',
@@ -50,7 +49,6 @@ self.addEventListener('fetch', function(event) {
   );
 });
 
-/*
 self.addEventListener('install', function(event) {
   console.log('install');
 });
@@ -68,3 +66,46 @@ self.addEventListener('fetch', function(event) {
   }
 });*/
 
+var CACHE_NAME = 'gih-cache-v3-clean-cache';
+var CACHED_URLS = [
+  'offline.html',
+  'mystyles.css',
+  'dino.png'
+];
+
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(CACHED_URLS);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get('accept').includes('text/html')) {
+          return caches.match('offline.html');
+        }
+      });
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (CACHE_NAME !== cacheName && cacheName.startsWith('gih-cache')) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
