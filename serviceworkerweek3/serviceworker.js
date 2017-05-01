4bf406316644a70882b3fc0678dc39881ddfd7f8
aedean <deanomonkey@hotmail.co.uk>
2017-05-01 23:38:42 +0100

    Update serviceworker.js
    big update with code off blackboard

--- serviceworkerweek3/serviceworker.js
+++ serviceworkerweek3/serviceworker.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'gih-cache-v5';
+/*var CACHE_NAME = 'gih-cache-v5';
 var CACHED_URLS = [
   // Our HTML
   'first.html',
@@ -46,3 +46,211 @@ self.addEventListener('activate', function(event) {
     })
   );
 });
+*/
+
+var BASE_PATH = '/NAMEOF REPRO/';
+var CACHE_NAME = 'gih-cache-v6';
+var TEMP_IMAGE_CACHE_NAME = 'temp-cache-v1';
+var newsAPIJSON = "https://newsapi.org/v1/articles?source=bbc-news&apiKey=d5a95d32f97b46069eb584f3b70385b8";
+
+var CACHED_URLS = [ 
+    //new
+    BASE_PATH + 'second.html',
+BASE_PATH + 'appimages/jack.jpg',
+BASE_PATH + 'appimages/news-default.jpg', 
+ BASE_PATH + 'appimages/event-default.png',
+BASE_PATH + 'scripts.js',
+BASE_PATH + 'events.json',
+
+    // Our HTML
+    BASE_PATH + 'first.html',
+    
+    // Images for favicons
+    BASE_PATH + 'appimages/android-icon-36x36.png',
+    BASE_PATH + 'appimages/android-icon-48x48.png',
+    BASE_PATH + 'appimages/android-icon-72x72.png',
+    BASE_PATH + 'appimages/android-icon-96x96.png',
+    BASE_PATH + 'appimages/android-icon-144x144.png',
+    BASE_PATH + 'appimages/android-icon-192x192.png',
+    BASE_PATH + 'appimages/favicon-32x32.png',
+
+    //Images for page
+    BASE_PATH + 'appimages/offlinemap.jpg',
+    BASE_PATH + 'appimages/dino.png',
+    BASE_PATH + 'appimages/jack.jpg',
+    BASE_PATH + 'appimages/paddy.jpg',
+    BASE_PATH + 'appimages/favicon.ico',
+    BASE_PATH + 'appimages/favicon-16x16.png',
+    BASE_PATH + 'appimages/favicon-32x32.png',
+    BASE_PATH + 'appimages/favicon-96x96.png',
+    BASE_PATH + 'appimages/ms-icon-70x70.png',
+    BASE_PATH + 'appimages/ms-icon-144x144.png',
+    BASE_PATH + 'appimages/ms-icon-150x150.png',
+    BASE_PATH + 'appimages/ms-icon-310x310.png',
+    BASE_PATH + 'eventsimages/example-blog01.jpg',
+    BASE_PATH + 'eventsimages/example-blog02.jpg',
+    BASE_PATH + 'eventsimages/example-blog03.jpg',
+    BASE_PATH + 'eventsimages/example-blog04.jpg',
+    BASE_PATH + 'eventsimages/example-blog05.jpg',
+    BASE_PATH + 'eventsimages/example-blog06.jpg',
+    BASE_PATH + 'eventsimages/example-blog07.jpg',
+    BASE_PATH + 'eventsimages/example-work01.jpg',
+    BASE_PATH + 'eventsimages/example-work02.jpg',
+    BASE_PATH + 'eventsimages/example-work03.jpg',
+    BASE_PATH + 'eventsimages/example-work04.jpg',
+    BASE_PATH + 'eventsimages/example-work05.jpg',
+    BASE_PATH + 'eventsimages/example-work06.jpg',
+    BASE_PATH + 'eventsimages/example-work07.jpg',
+    BASE_PATH + 'eventsimages/example-work08.jpg',
+    BASE_PATH + 'eventsimages/example-work09.jpg',  
+    // JavaScript
+    BASE_PATH + 'offline-map.js',
+    BASE_PATH + 'material.js',
+    // Manifest
+    BASE_PATH + 'manifest.json',
+  // CSS and fonts
+    'https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&lang=en',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    BASE_PATH + 'min-style.css',
+    BASE_PATH + 'styles.css'
+];
+
+var googleMapsAPIJS = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCMNj1opBAeNjTEY4PdjCABCwJdrHx0cvI&callback=initMap';
+
+self.addEventListener('install', function(event) {
+  // Cache everything in CACHED_URLS. Installation fails if anything fails to cache
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(CACHED_URLS);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  var requestURL = new URL(event.request.url);
+  // Handle requests for index.html
+  if (requestURL.pathname === BASE_PATH + 'first.html') {
+    event.respondWith(
+      caches.open(CACHE_NAME).then(function(cache) {
+        return cache.match('first.html').then(function(cachedResponse) {
+          var fetchPromise = fetch('first.html').then(function(networkResponse) {
+            cache.put('first.html', networkResponse.clone());
+            return networkResponse;
+          });
+          return cachedResponse || fetchPromise;
+        });
+      })
+    );
+ // Handle requests for Google Maps JavaScript API file
+  } else if (requestURL.href === googleMapsAPIJS) {
+    event.respondWith(
+      fetch(
+        googleMapsAPIJS+'&'+Date.now(),
+        { mode: 'no-cors', cache: 'no-store' }
+      ).catch(function() {
+        return caches.match('offline-map.js');
+      })
+    );
+  } else if (
+    CACHED_URLS.includes(requestURL.href) ||
+    CACHED_URLS.includes(requestURL.pathname)
+  ) // Handle requests for events JSON file
+  // Handle requests for events JSON file
+  } else if (requestURL.pathname === BASE_PATH + 'events.json') {
+    event.respondWith(
+      caches.open(CACHE_NAME).then(function(cache) {
+        return fetch(event.request).then(function(networkResponse) {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        }).catch(function() {
+          return caches.match(event.request);
+        });
+      })
+    );
+  } else if (requestURL.href === newsAPIJSON) {
+    event.respondWith(
+      caches.open(CACHE_NAME).then(function(cache) {
+        return fetch(event.request).then(function(networkResponse) {
+          cache.put(event.request, networkResponse.clone());
+          caches.delete(TEMP_IMAGE_CACHE_NAME);
+          return networkResponse;
+        }).catch(function() {
+          return caches.match(event.request);
+        });
+      })
+    );
+  // Handle requests for event images.
+  } else if (requestURL.pathname.includes('/eventsimages/')) {
+    event.respondWith(
+      caches.open(CACHE_NAME).then(function(cache) {
+        return cache.match(event.request).then(function(cacheResponse) {
+          return cacheResponse||fetch(event.request).then(function(networkResponse) {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          }).catch(function() {
+            return cache.match('appimages/event-default.png');
+          });
+        });
+      })
+    );
+  // 
+  } else if (requestURL.href.includes('bbci.co.uk/news/')) {
+    event.respondWith(
+      caches.open(TEMP_IMAGE_CACHE_NAME).then(function(cache) {
+        return cache.match(event.request).then(function(cacheResponse) {
+          return cacheResponse||fetch(event.request, {mode: 'no-cors'}).then(function(networkResponse) {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          }).catch(function() {
+            return cache.match('appimages/news-default.jpg');
+          });
+        });
+      })
+    );
+
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName.startsWith('gih-cache') && CACHE_NAME !== cacheName) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+const askJackForm = document.getElementById("askJackForm");
+if(askJackForm){
+    const askJackFormSubmit = e => {
+        e.preventDefault();
+        const contact = {
+            "name": e.target.name.value,
+            "email": e.target.email.value,
+            "question": e.target.question.value,
+        };
+
+        fetch("http://jackcat.co.uk/", {mode: "no-cors", method: "POST", body: contact})
+            .then(response => {
+                console.log('hello response!', response);
+            }).catch(() => {
+                let allContacts = [];
+
+                const existingContacts = localStorage.getItem('contact');
+                if(existingContacts){
+                    allContacts = JSON.parse(existingContacts);
+                }
+
+                allContacts.push(contact);
+                localStorage.setItem('contact', JSON.stringify(allContacts));
+            });
+    };
+    askJackForm.addEventListener('submit', askJackFormSubmit, false);
+}
+
+
+// news api - d5a95d32f97b46069eb584f3b70385b8
+
