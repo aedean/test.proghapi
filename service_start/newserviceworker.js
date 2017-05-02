fd7b57c00ae172e9e692a0b07498e2894f4e8e0b
aedean <deanomonkey@hotmail.co.uk>
2017-05-02 16:30:20 +0100

    Update newserviceworker.js

--- service_start/newserviceworker.js
+++ service_start/newserviceworker.js
@@ -58,9 +58,11 @@ var googleMapsAPIJS = 'https://maps.googleapis.com/maps/api/js?key=YOURKEY&callb
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(CACHED_URLS);
+    })
   );
 });
-
 
 self.addEventListener('fetch', function(event) {
   var requestURL = new URL(event.request.url);
