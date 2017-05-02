650dce354a39ddeb73f61f2e6d54496396a797f2
aedean <deanomonkey@hotmail.co.uk>
2017-05-02 14:23:45 +0100

    Update servicework2.js

--- service_start/servicework2.js
+++ service_start/servicework2.js
@@ -18,22 +18,17 @@ self.addEventListener('install', function(event) {
    console.log("installing..");
 });
 
-
 self.addEventListener('fetch', function(event) {
   var requestURL = new URL(event.request.url);
   if (requestURL.pathname === 'first.html') {
     event.respondWith(
       caches.open(CACHE_NAME).then(function(cache) {
         return cache.match('first.html').then(function(cachedResponse) {
-          var fetchPromise = fetch('first.html').then(function(networkResponse) {
-            cache.put('first.html', networkResponse.clone());
-            return networkResponse;
-          });
-          
-        });
-      })
-        return cachedResponse || fetchPromise;
-    );
+        return cachedResponse;
+        }
+      )}   
+    )
+        );
   } else if (
     CACHED_URLS.includes(requestURL.href) ||
     CACHED_URLS.includes(requestURL.pathname)) {
@@ -49,6 +44,33 @@ self.addEventListener('fetch', function(event) {
 });
 
 /*
+self.addEventListener('fetch', function(event) {
+  var requestURL = new URL(event.request.url);
+  if (requestURL.pathname === 'first.html') {
+    event.respondWith(
+      caches.open(CACHE_NAME).then(function(cache) {
+        return cache.match('first.html').then(function(cachedResponse) {
+          var fetchPromise = fetch('first.html').then(function(networkResponse) {
+            cache.put('first.html', networkResponse.clone());
+            return networkResponse;
+          });
+        });
+      })
+    );
+  } else if (
+    CACHED_URLS.includes(requestURL.href) ||
+    CACHED_URLS.includes(requestURL.pathname)) {
+    console.log("We arent loading first.html");
+    event.respondWith(
+      caches.open(CACHE_NAME).then(function(cache) {
+        return cache.match(event.request).then(function(response) {
+          return response || fetch(event.request);
+        })
+      })
+    );
+  }
+});
+
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     fetch(event.request).catch(function() {
